4b95062ca8063c9945916d84f7eabad7145d7524
Nova <lady.nova@gmail.com>
2023-02-17 22:07:47 +0800

    fix

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>蝦農人生訂購頁</title>
     <link href="vendor/css/bootstrap.css" rel="stylesheet">
-
+    <link rel="stylesheet" href="vendor/css/all.css" type="text/css" />
     <!-- Custom styles for this template -->
     <link href="vendor/css/features.css" rel="stylesheet">
 </head>
@@ -15,7 +15,7 @@
 
 @yield('main')
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-<script src="vendor/js/bootstrap.js"></script>
+<script src="vendor/js/bootstrap.bundle.min.js"></script>
 <script sec="vendor/js/twzipcode.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="vendor/js/main.js"></script>
