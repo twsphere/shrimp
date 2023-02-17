f9bdb7bd01695f1b9e698aacbf892540ec764173
NovaZhang <lady.nova@gmail.com>
2023-02-17 11:36:37 +0800

    add zipcode

---
+++ resources/views/base.blade.php
@@ -0,0 +1,23 @@
+<html lang="en"><head>
+    <meta charset="UTF-8">
+    <meta name="Generator" content="EditPlus">
+    <meta name="Author" content="">
+    <meta name="Keywords" content="">
+    <meta name="Description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>蝦農人生訂購頁</title>
+    <link href="vendor/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="vendor/css/features.css" rel="stylesheet">
+</head>
+<body>
+
+@yield('main')
+
+<script src="vendor/js/bootstrap.js"></script>
+<script sec="vendor/js/twzipcode.min.js"></script>
+
+</body>
+
+</html>
